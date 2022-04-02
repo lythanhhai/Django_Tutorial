1faad629f0fd6ed9661fdfe05ee2610755f434c2
lythanhhai <you@example.com>
2022-04-02 17:20:56 +0700

    update

---
+++ README.md
@@ -0,0 +1,2 @@
+# Django_Tutorial
+- Link: https://www.youtube.com/watch?v=F5mRW0jo-U4&t=332s
--- my_django_tutorial/pages/views.py
+++ my_django_tutorial/pages/views.py
@@ -6,13 +6,18 @@ def homepage_view(request, *arg, **kwarg):
     print(request)
     print(*arg, **kwarg, sep=" ")
     #return HttpResponse("<h1>Hello world</h1>")
-    return render(request, 'home.html', {})
+    return render(request, 'home.html', {'hello': 'hello world'})
 
-def contact_view(*arg, **kward):
-    return HttpResponse("<h1>Contact</h1>")
+def contact_view(request, *arg, **kward):
+    return render(request, "contact.html", {})
 
-def about_view(*arg, **kwarg):
-    return HttpResponse("<h1>About</h1>")
+def about_view(request, *arg, **kwarg):
+    my_context = {
+        'my_text': "this is a text",
+        'my_number': 23,
+        'my_list': [1, 2, 3, 4, 5]
+    }
+    return render(request, 'about.html', my_context)
 
 def social_view(*arg, **kward):
     return HttpResponse("<h1>Social</h1>")
