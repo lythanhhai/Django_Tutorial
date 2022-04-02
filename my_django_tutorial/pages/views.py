645b650868c39ad7ce0caa8e6a033f6c646c5386
lythanhhai <you@example.com>
2022-04-02 16:39:12 +0700

    :rocket: study basic template engine in django(show data, for, if, tags and filter)

--- my_django_tutorial/pages/views.py
+++ my_django_tutorial/pages/views.py
@@ -1,4 +1,3 @@
-from webbrowser import get
 from django.shortcuts import render
 from django.http import HttpResponse
 
@@ -7,13 +6,18 @@ def homepage_view(request, *arg, **kwarg):
     print(request)
     print(*arg, **kwarg, sep=" ")
     #return HttpResponse("<h1>Hello world</h1>")
-    return render(request, 'home.html', {})
+    return render(request, 'home.html', {'hello': 'hello world'})
 
 def contact_view(request, *arg, **kward):
     return render(request, "contact.html", {})
 
 def about_view(request, *arg, **kwarg):
-    return render(request, 'about.html', {})
+    my_context = {
+        'my_text': "this is a text",
+        'my_number': 23,
+        'my_list': [1, 2, 3, 4, 5]
+    }
+    return render(request, 'about.html', my_context)
 
 def social_view(*arg, **kward):
     return HttpResponse("<h1>Social</h1>")
